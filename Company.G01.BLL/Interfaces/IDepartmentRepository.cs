8de47a27ef3b2f4c0e10a625fd4b7e92ff187cd3
MoazMedhat <moazmedhatm7@gmail.com>
2025-09-23 01:27:52 +0300

    implementation of BLL and DAL

---
+++ Company.G01.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using Company.G01.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G01.BLL.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+
+        Department? Get(int id);
+        int add(Department model);
+        int update(Department model);
+        int delete(Department model);
+    }
+}
