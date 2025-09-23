dfc7edc727d8db786a2b3355c3b0c99f59ca3dc4
MoazMedhat <moazmedhatm7@gmail.com>
2025-09-23 20:02:06 +0300

    Create Create(Action)

---
+++ Company.G01.PL/Dtos/CreatedepartmentDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.G01.PL.Dtos
+{
+    public class CreatedepartmentDto
+    {
+        [Required(ErrorMessage ="Code is required !")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Name is required !")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Date is required !")]
+        public DateTime CreateAt { get; set; }
+    }
+}
