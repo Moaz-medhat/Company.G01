03f223edd4a2f3a1d8c12b6a97d16594e051c463
MoazMedhat <moazmedhatm7@gmail.com>
2025-09-30 15:34:18 +0300

    implementation of PL

---
+++ Company.G01.PL/Controllers/EmployeeController.cs
@@ -0,0 +1,134 @@
+﻿using Company.G01.BLL.Interfaces;
+using Company.G01.DAL.Models;
+using Company.G01.PL.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.G01.PL.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _EmployeeRepository;
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _EmployeeRepository = employeeRepository;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var employees = _EmployeeRepository.GetAll();
+            return View(employees);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateEmployeeDto employeeDto)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var employee = new Employee()
+                {
+                   Name=employeeDto.Name,
+                   Adress=employeeDto.Adress,
+                   Age=employeeDto.Age,
+                   CreateAt=employeeDto.CreateAt,
+                   HiringDate=employeeDto.HiringDate,
+                   Email=employeeDto.Email,
+                   IsActive=employeeDto.IsActive,
+                   IsDeleted=employeeDto.IsDeleted,
+                   Phone=employeeDto.Phone,
+                   Salary=employeeDto.Salary,
+                };
+                var count = _EmployeeRepository.add(employee);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(employeeDto);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id, string viewName = "Details")
+        {
+            if (id is null) return BadRequest("Invalid Id");
+
+            var employee = _EmployeeRepository.Get(id.Value);
+            if (employee is null) return NotFound(new { StatusCode = 404, message = $"Employee with id : {id} not found" });
+
+            return View(viewName, employee);
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            //if (id is null) return BadRequest("Invalid Id");
+
+            //var department = _departmentRepository.Get(id.Value);
+            //if (department is null) return NotFound(new { StatusCode = 404, message = $"department with id : {id} not found" });
+
+            return Details(id, "Edit");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int id, Employee employee)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                if (id != employee.Id) return BadRequest();
+
+
+                var count = _EmployeeRepository.update(employee);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+
+
+            return View(employee);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            //if (id is null) return BadRequest("Invalid Id");
+
+            //var department = _departmentRepository.Get(id.Value);
+            //if (department is null) return NotFound(new { StatusCode = 404, message = $"department with id : {id} not found" });
+
+            return Details(id, "Delete");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete([FromRoute] int id, Employee employee)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                if (id != employee.Id) return BadRequest();
+
+
+                var count = _EmployeeRepository.delete(employee);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+
+
+            return View(employee);
+        }
+
+    }
+}
---
+++ Company.G01.PL/Dtos/CreateEmployeeDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Company.G01.PL.Dtos
+{
+    public class CreateEmployeeDto
+    {
+        [Required(ErrorMessage ="Name is Required")]
+        public string Name { get; set; }
+        [Range(20,80, ErrorMessage ="Age must be between 20 & 80 ")]
+        public int? Age { get; set; }
+        [DataType(DataType.EmailAddress, ErrorMessage = "Email is not valid")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Adress is Required")]
+        public string Adress { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Salary { get; set; }
+
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        [DisplayName("Hiring date")]
+        public DateTime HiringDate { get; set; }
+        [DisplayName("Date of create")]
+
+        public DateTime CreateAt { get; set; }
+
+    }
+}
